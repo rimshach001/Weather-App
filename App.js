f004f111230d43bc87d9c21ec413a8c277de0d07
rimshach001 <rimshach312@gmail.com>
2023-09-03 11:01:04 -0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { View, Text } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import WeatherScreen from './src/screens/WeatherScreen/weatherScreen'
+import { Provider } from 'react-redux'
+import store from './src/redux/Store/Store'
+const App = () => {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Provider store={store}>
+    <WeatherScreen/>
+    </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
